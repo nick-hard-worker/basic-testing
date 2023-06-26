7800dec2356abb967fe95d204b522d423a5b83f0
NickUA <246246s@gmail.com>
2023-06-26 13:01:05 +0300

    test: implement test simpleCalculator

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,65 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    let result = simpleCalculator({ a: 5, b: 8, action: Action.Add });
+    expect(result).toBe(13);
+    result = simpleCalculator({ a: -3, b: -10, action: Action.Add });
+    expect(result).toBe(-13);
+    result = simpleCalculator({ a: 6.4, b: 0.6, action: Action.Add });
+    expect(result).toBeCloseTo(7);
   });
 
   test('should substract two numbers', () => {
-    // Write your test here
+    let result = simpleCalculator({ a: 5, b: 8, action: Action.Substract });
+    expect(result).toBe(-3);
+    result = simpleCalculator({ a: -3, b: -10, action: Action.Substract });
+    expect(result).toBe(7);
+    result = simpleCalculator({ a: 6.4, b: 0.6, action: Action.Substract });
+    expect(result).toBeCloseTo(5.8);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    let result = simpleCalculator({ a: 5, b: 8, action: Action.Multiply });
+    expect(result).toBe(40);
+    result = simpleCalculator({ a: -3, b: -10, action: Action.Multiply });
+    expect(result).toBe(30);
+    result = simpleCalculator({ a: 6.4, b: 0.6, action: Action.Multiply });
+    expect(result).toBeCloseTo(3.84, 8);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    let result = simpleCalculator({ a: 8, b: 2, action: Action.Divide });
+    expect(result).toBe(4);
+    result = simpleCalculator({ a: -5, b: -2, action: Action.Divide });
+    expect(result).toBeCloseTo(2.5, 8);
+    result = simpleCalculator({ a: 6.4, b: -0.2, action: Action.Divide });
+    expect(result).toBeCloseTo(-32, 8);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    let result = simpleCalculator({ a: 8, b: 2, action: Action.Exponentiate });
+    expect(result).toBe(64);
+    result = simpleCalculator({ a: -5, b: -2, action: Action.Exponentiate });
+    expect(result).toBeCloseTo(0.04, 8);
+    result = simpleCalculator({ a: 6.4, b: -0.2, action: Action.Exponentiate });
+    expect(result).toBeCloseTo(0.69, 3);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 8, b: 2, action: 'someAction' });
+    expect(result).toBeNull;
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    let result = simpleCalculator({ a: '8', b: 2, action: Action.Add });
+    expect(result).toBeNull;
+    result = simpleCalculator({ a: 8, b: 'digit', action: Action.Multiply });
+    expect(result).toBeNull;
+    result = simpleCalculator({ a: null, b: 3, action: Action.Multiply });
+    expect(result).toBeNull;
+    result = simpleCalculator({ a: undefined, b: 3, action: Action.Substract });
+    expect(result).toBeNull;
   });
 });
